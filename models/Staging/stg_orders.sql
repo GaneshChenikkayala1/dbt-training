3958e2edd0d098fe2561736127c93d79a5e0b354
ganeshchenikkayala1@gmail.com <ganeshchenikkayala1@gmail.com>
2023-06-09 04:24:50 +0000

    Created a macro

--- models/Staging/stg_orders.sql
+++ models/Staging/stg_orders.sql
@@ -22,7 +22,8 @@ country,
 category,
 productname,
 subcategory,
-ordersellingprice - ordercostprice as orderprofit
+ordersellingprice - ordercostprice as orderprofit,
+{{markup()}} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customers') }} as c
 on o.customerid = c.customerid
